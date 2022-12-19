6ea6c2e82f5896fc0cfcd4273e845ae32ac46ccc
Vladislav Krainev <vladislav.krainev@gmail.com>
2022-12-19 17:43:31 +0200

    Responsive Projects Slider

--- main.js
+++ main.js
@@ -6,21 +6,21 @@ let distance = 0;
 
 // Will change sausage's right property, therefore moving it left or right
 function moveSausage() {
-  sausage.style.right = `${distance}px`;
+  sausage.style.right = `${distance}%`;
 }
 
 // Will reduce distance, subsequently moving sausage to the right if it's is not already 0
 function reduceDistance() {
   if (distance !== 0) {
-    distance -= 1000;
+    distance -= 100;
     moveSausage();
   }
 }
 
 // Will increase distance, subsequently moving sausage to the left if it's not already 3000
 function increaseDistance() {
-  if (distance < 3000) {
-    distance += 1000;
+  if (distance < 300) {
+    distance += 100;
     leftArrow.style.opacity = "1";
     moveSausage();
   }
@@ -28,9 +28,9 @@ function increaseDistance() {
 
 // Changes the opacity of right/left arrows
 function changeOpacity(e) {
-  if (distance === 2000 && e.target.id === "right_arrow") {
+  if (distance === 200 && e.target.id === "right_arrow") {
     rightArrow.style.opacity = "0.2";
-  } else if (distance === 1000 && e.target.id === "left_arrow") {
+  } else if (distance === 100 && e.target.id === "left_arrow") {
     leftArrow.style.opacity = "0.2";
   } else if (distance !== 0 && e.target.id === "left_arrow") {
     rightArrow.style.opacity = "1";
